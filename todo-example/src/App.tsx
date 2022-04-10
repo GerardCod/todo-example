090af0f50bee76377835cea500bf2e1f83e09c31
Gera <gerardo.aguilar.calderon@gmail.com>
2022-04-09 19:03:18 -0500

    Cambiando la etiqueta p por h1 en el componente principal

--- todo-example/src/App.tsx
+++ todo-example/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hola mundo</p>
+        <h1>Hola mundo</h1>
       </header>
     </div>
   )
