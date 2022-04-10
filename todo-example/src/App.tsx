c97abedb63313348c5ef4d842b80e1ba5f457ef2
Gera <gerardo.aguilar.calderon@gmail.com>
2022-04-09 19:09:34 -0500

    Pasando el mensaje del componente App a francés.

--- todo-example/src/App.tsx
+++ todo-example/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hola mundo</h1>
+        <h1>Salut tout le monde</h1>
       </header>
     </div>
   )
